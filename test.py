5f9128ff3b2f21ea8f8463dc5443fe28d49af5ba
aslam <aslam@alethea.in>
2023-12-01 11:08:42 +0530

    with comments

--- test.py
+++ test.py
@@ -1,52 +1,60 @@
-from bardapi import Bard
-import os
-import requests
-import json
 
-os.environ['_BARD_API_KEY']="dQh7a2VL72hAjugeTpFd5Nbqj6rDSHK1Lyh-tRBnlyY_593F3uD1xFH-UxkCnbyvw1fSsw."
+```python
+from bardapi import Bard  # Import the Bard API library
+import os  # Import the os module for file system operations
+import requests  # Import the requests library for making HTTP requests
+import json  # Import the json module for working with JSON data
+
+# Set the Bard API key as an environment variable
+os.environ['_BARD_API_KEY'] = "dQh7a2VL72hAjugeTpFd5Nbqj6rDSHK1Lyh-tRBnlyY_593F3uD1xFH-UxkCnbyvw1fSsw."
 
 def Comment(file_path):
-    
+    # Initialize a requests session
     session = requests.Session()
-    session.headers = {
-                "Host": "bard.google.com",
-                "X-Same-Domain": "1",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-                "Origin": "https://bard.google.com",
-                "Referer": "https://bard.google.com/",
-            }
-    session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY")) 
 
+    # Set the HTTP headers for the requests
+    session.headers = {
+        "Host": "bard.google.com",
+        "X-Same-Domain": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Origin": "https://bard.google.com",
+        "Referer": "https://bard.google.com/",
+    }
+
+    # Set the Bard API key as a cookie in the session
+    session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY"))
+
+    # Create a Bard instance with the API key and session
     bard = Bard(token=os.environ['_BARD_API_KEY'], session=session, timeout=30)
 
+    # Continuously loop until the user exits
     while True:
-        # message = str(input("Prompt:[COMMENT|DOCUMENT: ] "))
-        message = "comment"
-        _message = message.lower()
-        if _message=="comment":
-            _code = open('./gpt.py')
-            code = _code.read()
-            _code.close()
-            prompt ="Comment the code above"
-            final_message = str(code)+"\n"+prompt
+        # Prompt the user to enter a message
+        message = str(input("Prompt:[COMMENT|DOCUMENT: ] "))
+
+        # Check if the message is "comment"
+        if message.lower() == "comment":
+            # Read the code from the file
+            with open('./gpt.py') as code_file:
+                code = code_file.read()
+
+            # Prepare the prompt for Bard
+            prompt = "Comment the code above"
+            final_message = str(code) + "\n" + prompt
+
+            # Send the request to Bard and get the response
             try:
                 answer = bard.get_answer(final_message)
                 final_answer = answer['content'].split("\n")
-                return final_answer,True
-                # with open("./output.py",'w') as write_file:
-                #     store=answer['content'].split("\n")
-                #     for i in range(1,len(store)):
-                #         write_file.write(store[i]+"\n")
+                return final_answer, True
             except Exception as e:
-                return "",False
-            
+                print(e)
+                return "", False
 
-            
         else:
-            return "",False
+            # If the message is not "comment", return an empty string and False
+            return "", False
 
-
-
-
-print("Hello world")
+print("Hello world")  # Print the greeting message
+```
