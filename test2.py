8be7458d3c6152dcb30f353337b626baec4cc3e8
aslam <aslam@alethea.in>
2023-12-01 12:35:07 +0530

    lsjdlfj

--- test2.py
+++ test2.py
@@ -1,8 +1,16 @@
-
 def function():
     # This function prints "Hello, world!" to the console.
-    print("Hello, world!")
+    something = True
+
+    # Check if the value of 'something' is True
+    if something:
+        # If 'something' is True, print "Hello, world!"
+        print("Hello, world!")
+    else:
+        # If 'something' is not True, print 'nothing'
+        print('nothing')
 
 if __name__ == "__main__":
     # Calls the function to print the message.
     function()
+
